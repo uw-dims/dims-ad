ce9c5bc8e9c31ad368ff7511aeb8f98b613c681c
Dave Dittrich <dittrich@u.washington.edu>
2016-06-10 19:36:37 -0700

    Fix syntax error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -389,8 +389,8 @@ if on_rtd:
             'dimsasbuilt': ("https://dims-asbuilt.readthedocs.io/en/{0}".format(git_tag), None),
             'dimsocd': ("https://dims-ocd.readthecods.io/en/{0}".format(git_tag), None),
             'dimsjds': ("https://dims-jds.readthedocs.io/en/{0}".format(git_tag), None),
-            'dimssr': ("https://dims-sr.readthedocs.io/en/{0}".format(git_tag), , None),
-            'dimstp': ("https://dims-tp.readthedocs.io/en/{0}".format(git_tag), , None),
+            'dimssr': ("https://dims-sr.readthedocs.io/en/{0}".format(git_tag), None),
+            'dimstp': ("https://dims-tp.readthedocs.io/en/{0}".format(git_tag), None),
 else:
     intersphinx_mapping = {
             'dimsasbuilt': ("{}/dims-asbuilt".format(os.environ['DOCSURL']), None),
