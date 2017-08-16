cc95557bde3e6f8fa3009662337d332fc9f99df0
Dave Dittrich <dittrich@u.washington.edu>
2017-08-16 15:56:54 -0700

    Update copyright date

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DIMS Architecture Design'
-copyright = u'2014-2016 University of Washington'
+copyright = u'2014-2017 University of Washington'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -310,7 +310,7 @@ git_tag = os.environ.get('GITTAG', "latest")
 epub_title = u'DIMS Architecture Design'
 epub_author = u'David Dittrich, Stuart Maclean'
 epub_publisher = u'University of Washington'
-epub_copyright = u'2014, 2016 University of Washington'
+epub_copyright = u'2014-2017 University of Washington'
 
 # The basename for the epub file. It defaults to the project name.
 #epub_basename = u'DIMS Architecture Design'
